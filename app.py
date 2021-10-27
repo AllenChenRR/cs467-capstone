66f47ec3bd5e41593a8a84bc622d4eb0b63d1345
Jamie Smead <jdsmead@outlook.com>
2021-10-26 20:33:11 -0400

    Combining branches manually

--- app.py
+++ app.py
@@ -7,10 +7,11 @@
 
 from datetime import datetime
 from firebase_admin import credentials, firestore, initialize_app
-from flask import Flask, jsonify, redirect, request, session
+from flask import Flask, jsonify, redirect, request, session, render_template, flash, url_for
 from flask.helpers import url_for
 import password as pw
 import usermodel as model
+from forms import RegistrationForm, LoginForm
 
 # Initialize Flask app
 app = Flask(__name__)
@@ -18,95 +19,117 @@ app = Flask(__name__)
 # Set secret key for sessions
 app.secret_key = "sdkfjDCVBsdjKkl%@%23$"
 
+# wtf form file
+# app.config['SECRET_KEY'] = '1f126886d424206b9b80a69066bf3f8f'
+
 # Initialize Firestore DB
-cred = credentials.Certificate('serviceAccountKey.json')
+# cred = credentials.Certificate('serviceAccountKey.json')
+cred = credentials.Certificate('Key.json') # jamie's db
 default_app = initialize_app(cred)
 db = firestore.client()
 
 
 def get_user_id(req):
     """
-    Returns the unique ID from a specific document in the users collection
-    """
-    # Create a reference to the users collection
-    user_ref = db.collection('users')
+    Returns the unique ID from a specific document in the users collection or an int if the user is not found.
 
-    # Query the "users" collection for the document with the matching email
+    """
+    user = get_user(req)
+    return user if isinstance(user, int) else user.id
+
+def get_user(req):
+    """
+    Returns the unique user document in the users collection
+    """
+    user_ref = db.collection('Users')
     query_ref = user_ref.where('email', '==', req['email'])
-    all = list(query_ref.stream())  # is ther ea better way of doing this?
-    if len(all) != 1:
-        # something went wrong
-        return len(all)
-    else:
-        # There must be excatly one document in the stream
-        # print(all[0].id)
-        return all[0].id
+    all = list(query_ref.stream())
+    return len(all) if len(all) != 1 else all[0] 
 
 
-@app.route('/', methods=['GET'])
+@app.route("/", methods=['GET'])
+@app.route("/home")
 def home():
-    """
-    Test landing page
-    """
-    return "This is the landing page for now."
+    return render_template("home.html")
 
 
-@app.route('/sign_up', methods=['POST'])
-def sign_up():  # this could also be used to GET all profiles if we need it
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    form = RegistrationForm()
+    user_ref = db.collection('Users')
+    if form.validate_on_submit():
 
-    """
-        sign_up() : Add document to Firestore collection with request body.
-        e.g. json={
-            'first': 'John',
-            'last': 'Smith',
-            'password': 'abc123'}
+        request_dict = {}
+        password = form.password.data
+        hash_result = pw.return_salt_hash(password) 
+        request_dict["first_name"] = form.first_name.data
+        request_dict["last_name"] = form.last_name.data
+        request_dict["email"] = form.email.data
+        request_dict["salt"] = hash_result[0]
+        request_dict["hash"] = hash_result[1]
+        request_dict["is_admin"] = False
+        request_dict["last_update"] = datetime.now()
+        
+        user_ref.document().set(request_dict)
 
-        Notes: We could also use a custom ID if needed
-    """
-    user_ref = db.collection('users')
-    if request.method == 'POST':  # will there only be post on the route?
-        try:
-            request_dict = request.json
-            request_dict["admin"] = False
-            request_dict["last_update"] = datetime.now()
-            user_ref.document().set(request_dict)
-            return {"success": True}, 200
-        except Exception as e:
-            return f"An Error Occured: {e}"
-    else:
-        return {"error": "Method Not Allowed"}, 405
+        flash(f'Account created for { form.email.data }', 'success')
+        return redirect(url_for('home'))
+    
+    return render_template("signup.html", title="Sign Up", form=form)
+
+# ignore this for now...
+# # break this up?
+# @app.route('/account', methods=['DELETE', 'POST', 'PATCH'])
+# def user_account():
+#     """
+#     Not final but works
+#     Does it make sense to have this one route do everything?
+#     """
+#     try:
+#         user_ref = db.collection('users')
+#         user_id = get_user_id(request.json)
+#         if not isinstance(user_id, int):
+#             if request.method == 'POST':
+#                 user = user_ref.document(user_id).get()
+#                 return jsonify(user.to_dict()), 200
+#             if request.method == 'DELETE':
+#                 user_ref.document(user_id).delete()
+#                 return "", 204
+#             # elif request.method == 'PATCH': ### not working yet
+#             #     user_ref.document(user_id).update(request.json, merge=True) # need to validate the request json
+#                 return "updated", 200
+#             else:
+#                 return {"error": "Method Not Allowed"}, 405
+#         else:
+#             if not user_id:
+#                 return {"error": "email does not exist"}, 400
+#             else:
+#                 return {"error": "More than one account with that email"}, 400
+
+#     except Exception as e:
+#         return f"An Error Occured: {e}"
 
 
-# break this up?
-@app.route('/account', methods=['DELETE', 'POST', 'PATCH'])
-def user_account():
-    """
-    Not final but works
-    Does it make sense to have this one route do everything?
-    """
-    try:
-        user_ref = db.collection('users')
-        user_id = get_user_id(request.json)
-        if not isinstance(user_id, int):
-            if request.method == 'POST':
-                user = user_ref.document(user_id).get()
-                return jsonify(user.to_dict()), 200
-            if request.method == 'DELETE':
-                user_ref.document(user_id).delete()
-                return "", 204
-            # elif request.method == 'PATCH': ### not working yet
-            #     user_ref.document(user_id).update(request.json, merge=True) # need to validate the request json
-                return "updated", 200
-            else:
-                return {"error": "Method Not Allowed"}, 405
-        else:
-            if not user_id:
-                return {"error": "email does not exist"}, 400
-            else:
-                return {"error": "More than one account with that email"}, 400
 
-    except Exception as e:
-        return f"An Error Occured: {e}"
+
+
+
+# @app.route("/login", methods=["GET", "POST"])
+# def login():
+#     form = LoginForm()
+    
+#     if form.validate_on_submit(): # this is checking that the data in the form was valid, hasn't checked against DB yet
+#         login_valid = True # this condition can be changed 
+#         if login_valid:
+#             flash(f'Welcome, {form.email.data}', 'success') # can change to first name later
+#             # TODO: You can access the form data with the variables form.email.data 
+#             # and form.password.data to check if it's valid, then do whatever you need to do with the DB here
+#             return redirect(url_for('home'))
+#         else:
+#             # in case of an unsuccessful login
+#             flash('Login unsuccessful. Please try again.', 'danger')
+
+#     return render_template("login.html", title="Login To Your Account", form=form)
 
 
 # Handles logging in and logging out
---
+++ forms.py
@@ -0,0 +1,22 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+# This class was built with the assistance of the following tutorial: 
+# "Python Flask Tutorial: Full-Featured Web App Part 3 - Forms and User Input"
+# Link: https://youtu.be/UIJKdCIEXUQ
+
+class RegistrationForm(FlaskForm):
+    first_name = StringField('First Name', validators=[DataRequired(), Length(min=2, max=20)])
+    last_name = StringField('Last Name', validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=4, max=18)])
+    confirm_password = PasswordField('Confirm Password', 
+                                        validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+class LoginForm(FlaskForm):
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password',
+                             validators=[DataRequired(), Length(min=4)])
+    submit = SubmitField('Login')
